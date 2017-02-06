238aed128677bc953e972cc60337844748ed5ea9
ragoolaman <ragoolaman01@gmail.com>
2017-02-05 19:20:29 -0700

    added the first several labs for completions sake

---
+++ CiscoCppFiles/Lab 1-2-6/Lab.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+	cout << "It's me, your first program.";
+	getchar();
+	/*Getchar() allows the command line in visual studio to stay open until you hit enter and actually show the output for the program
+	you just ran, other wise it will start and end too fast for one to see. For proof that I am not an idiot, please remove the line 
+	and test this for yourself.
+	Then, put it back to allow you to see. This explanation will only be added once.*/
+	return 0;
+}
---
+++ CiscoCppFiles/Lab 1-3-1/Lab.cpp
@@ -0,0 +1,12 @@
+#include <iostream>//we included iostream
+#include <iomanip>//we included iomanip
+#include <string>//we included string
+int main()
+{
+	int v = 10800;
+	int zzz = 3 * 60;
+	int zzz = 5 * 60;
+	float siii = 3.141526; 
+	//std::cout << "result: " << result << std::endl;
+	//^ unused line of code b/c not enough time to write it
+}

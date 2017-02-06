88c8b69c1dd5b134c984198a6930867d21810191
ragoolaman <ragoolaman01@gmail.com>
2017-02-05 21:24:31 -0700

    Tried a ton of stuff for 1-4-40, no luck yet

--- CiscoCppFiles/Lab 1-4-40/Lab.cpp
+++ CiscoCppFiles/Lab 1-4-40/Lab.cpp
@@ -3,15 +3,23 @@ using namespace std;
 int main(void) {
 	float pi = 3.14159265359;
 	float x, y;
+	float a, b, c, d;
 	cout << "Enter value for x: ";
 	cin >> x;
 	// put your code here
-	y = ((x*x)/(pi * pi *((x * x) + (1/2))));
-	y *= (1 + ((x * x)/(pi * pi *((x*x - (1/2))*(x*x - (1 / 2))))));
-	cout << "y = " << y;
-	while (getchar()) 
+	//y = ((x*x)/(pi * pi *((x * x) + (1/2))));
+	//y *= (1 + ((x * x)/(pi * pi *((x*x - (1/2))*(x*x - (1 / 2))))));
+	/*a = x*x;
+	b = pi * pi *(x * x + (1/2));
+	c = x*x - (1/2);
+	c = c*c;
+	d = pi * pi *c;
+	y = (a/b)*(1+(a/d));
+	cout << "y = " << y;*/
+	//y = ((x*x/(pi*pi*(x*x + (1/2)))*(1+(x*x/(pi*pi*((x*x - (1/2))*(x*x - (1/2))))))));
+	/*while (getchar()) 
 	{
-	}
+	}*/
 	getchar();
 	return 0;
 }

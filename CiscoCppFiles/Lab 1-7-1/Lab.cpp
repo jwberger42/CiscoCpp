eab7492d44780e2940be93095d5b0099806120c7
John Berger <jwberger@shaw.ca>
2017-02-27 09:24:16 -0700

    Did lab 1.7.1, humourously.... You'll see!

---
+++ CiscoCppFiles/Lab 1-7-1/Lab.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	float a = 2.3;
+	float b = 2.3;
+	float c = 2.123456;
+	float d = 2.123456;
+	float e = 2.123456;
+	cout << "2.3" << endl;
+	cout << "2.30" << endl;
+	cout << "2.123456" << endl;
+	cout << "2.12" << endl;
+	cout << "2" << endl;
+	getchar();
+}

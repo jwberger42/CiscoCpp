dcd774f4de646be366b8ad0d75dbf4dbc479c994
John Berger <jwberger@shaw.ca>
2017-03-09 09:19:32 -0700

    Added chapter two, skipped a few 1.7's

---
+++ CiscoCppFiles/Lab 1-7-3/Lab.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) 
+{
+	int in1;
+	int in2;
+	int in3;
+	int in4;
+	cout << "Enter a number" << endl;
+	cin >> in1;
+	cout << "Enter another" << endl;
+	cin >> in2;
+	cout << "One more" << endl;
+	cin >> in3;
+	cout << "Last one" << endl;
+	cin >> in4;
+	cout << in1 << "." << in2 << "." << in3 << "." << in4 << endl;
+	while (true) {}
+}
---
+++ CiscoCppFiles/Lab 2-1-5/Lab.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	// Insert your code here
+	return 0;
+}
